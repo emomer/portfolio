7661ee480aefad4bd8603a42dab3c202abf90d64
emomer <emrecan03@live.de>
2025-08-10 11:20:18 +0200

    Build to docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/portfolio/", // Repo-Name mit führendem & abschließendem Slash
+  base: "/portfolio/",
+  build: { outDir: "docs" },
   plugins: [react(), tailwindcss()],
 });
